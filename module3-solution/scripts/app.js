59c1bdad9e5328d71b351c54e15947c2b31cb06d
Kakada Ly <lkakada@gmail.com>
2016-10-01 18:02:42 -0700

    modified files

--- module3-solution/scripts/app.js
+++ module3-solution/scripts/app.js
@@ -5,15 +5,7 @@
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
   .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-  .component('foundItems', {
-    templateUrl: 'foundItems.html',
-    bindings: {
-      items: '<',
-      title: '@',
-      onRemove: '&',
-      search: '<'
-    }
-  });
+  .directive('foundItems', FoundItems);
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController (MenuSearchService){
@@ -26,6 +18,7 @@
             menu.found = response;
             menu.title = (menu.found.length+ " item(s) found");
             menu.filter = searchTerm;
+            console.log(menu.found);
           })
           .catch(function(error){
             console.log("Connection Error")
@@ -39,13 +32,36 @@
       };
   }
 
+  function FoundItems() {
+  var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        items: '<',
+        onRemove: '&',
+        search: '<'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'list',
+      bindToController: true
+  };
+    return ddo;
+  }
+
+  function FoundItemsDirectiveController() {
+    var list = this;
+  }
+
   MenuSearchService.$inject = ['$http', 'ApiBasePath']
   function MenuSearchService($http, ApiBasePath) {
     var service = this;
 
     service.getMatchedMenuItems = function(searchTerm){
-      return $http({method: "GET", url: (ApiBasePath+"/menu_items.json")})
-        .then(function (response) {
+      return $http(
+          {
+            method: "GET",
+            url: (ApiBasePath+"/menu_items.json")
+          })
+          .then(function (response) {
           //process the result and only keep items that match
           var items = response.data.menu_items;
           var foundItems = [];
@@ -55,7 +71,6 @@
           } else {
             for(var i = 0; i < items.length; i++){
               var str = items[i].description;
-
               if(str.toLowerCase().indexOf(searchTerm) >= 0){
                 foundItems.push(items[i]);
               }
